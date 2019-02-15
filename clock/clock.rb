dc0e296512fc46ba362512066d6df93cd5116fe1
suchov <artem.suchov@gmail.com>
2019-02-15 18:06:45 +0200

    pause on clock and download difference of squares

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,5 +1,29 @@
 class Clock
-  def initialize(hour)
-    Time.at(hour.values.each)
+  def initialize(time_hash)
+    @hours = time_hash[:hour] || 0
+    @minutes  = time_hash[:minute] || 0
+    @internal_minutes = @hours * 60 + @minutes
+  end
+
+  def to_s
+    format '%02d:%02d', m_hours, m_minutes
+  end
+
+  def +(minutes)
+    puts minutes
+    puts @minutes
+    puts m_minutes
+    @minutes += minutes
+    self
+  end
+
+  private
+
+  def m_hours
+    @internal_minutes / 60 % 24
+  end
+
+  def m_minutes
+    @internal_minutes % 60
   end
 end
