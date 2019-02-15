c92ce98a99af144e290a6cbfac49fce293d34b5f
suchov <artem.suchov@gmail.com>
2019-02-15 21:26:49 +0200

    add solution back and force

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -3,3 +3,8 @@ class Complement
    dna.tr('GCTA', 'CGAU')
  end
 end
+
+#  def self.of_dna(dna)
+#    raise ArgumentError, 'Incorrect nucleotide' if dna.include? 'U'
+#    dna.tr('GCTA', 'CGAU')
+#  end
