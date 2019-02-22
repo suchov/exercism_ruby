26031f4d976c984f216fbdb77e5f47682739513f
suchov <artem.suchov@gmail.com>
2019-02-23 00:34:25 +0200

    stupid modification of space age test

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,14 +1,15 @@
 class SpaceAge
+  EARTH_YR = 31557600.0
 
   def initialize(age)
-    self.class.send(:define_method, 'on_earth') { (age / 31557600.0).round(2) }
-    self.class.send(:define_method, 'on_mercury') { (age / (0.2408467 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_venus') { (age / (0.61519726 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_neptune') { (age / (164.79132 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_uranus') { (age / (84.016846 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_saturn') { (age / (29.447498 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_jupiter') { (age / (11.862615 * 31557600.0)).round(2) }
-    self.class.send(:define_method, 'on_mars') { (age / (1.8808158 * 31557600.0)).round(2) }
+    self.class.send(:define_method, 'on_earth') { (age / EARTH_YR).round(2) }
+    self.class.send(:define_method, 'on_mercury') { (age / (0.2408467 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_venus') { (age / (0.61519726 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_neptune') { (age / (164.79132 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_uranus') { (age / (84.016846 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_saturn') { (age / (29.447498 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_jupiter') { (age / (11.862615 * EARTH_YR)).round(2) }
+    self.class.send(:define_method, 'on_mars') { (age / (1.8808158 * EARTH_YR)).round(2) }
   end
 
 end
