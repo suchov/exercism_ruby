3ac1407b86d1673a29dde031601ca2ae7540d993
suchov <artem.suchov@gmail.com>
2019-02-22 23:35:11 +0200

    finish the panagram

---
+++ pangram/pangram.rb
@@ -0,0 +1,13 @@
+class Pangram
+  def self.pangram?(sentence)
+    unless sentence.chomp.empty?
+      sentence.chomp!
+      sentence.downcase!
+      ('a'..'z').each do |l|
+        if sentence.index(l).nil?
+          return false
+        end
+      end
+    end
+  end
+end
