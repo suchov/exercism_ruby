059ee0598d5be853f08c5b6bd10d32f0cef5d6e9
suchov <artem.suchov@gmail.com>
2019-02-22 23:40:31 +0200

    find quicker solution

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,13 +1,14 @@
 class Pangram
   def self.pangram?(sentence)
-    unless sentence.chomp.empty?
-      sentence.chomp!
-      sentence.downcase!
-      ('a'..'z').each do |l|
-        if sentence.index(l).nil?
-          return false
-        end
-      end
-    end
+    # unless sentence.chomp.empty?
+    #   sentence.chomp!
+    #   sentence.downcase!
+    #   ('a'..'z').each do |l|
+    #     if sentence.index(l).nil?
+    #       return false
+    #     end
+    #   end
+    # end
+    ([*'a'..'z'] - sentence.downcase.chars).empty?
   end
 end
